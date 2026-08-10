aa57a10610b659625b7e01a8bf779720fb0ad7fc
Mildrette <mildredfonka1@gmail.com>
2026-08-10 20:31:56 +0100

    Add message input field and character counter using state and functions

---
+++ app/src/main/java/com/example/messagechecker/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.messagechecker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.messagechecker.ui.WelcomeView
+import com.example.messagechecker.ui.theme.MessageCheckerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MessageCheckerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    WelcomeView()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/messagechecker/ui/WelcomeView.kt
@@ -0,0 +1,82 @@
+package com.example.messagechecker.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.Divider
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+@Composable
+fun WelcomeView(){
+var textInput by remember { mutableStateOf(" ")}
+    
+    Row(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)
+        .background(Color.White)
+    ){
+        Column( modifier = Modifier
+            .padding(20.dp)
+
+        ){
+
+            Text("Message Checker",
+                modifier = Modifier.padding(16.dp),
+                color = Color.Gray,
+                fontSize = 25.sp
+
+            )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+              value = textInput,
+                onValueChange = {textInput = it},
+                label = { Text("Enter Your Message Here")}
+            )
+
+            Spacer( modifier = Modifier.height(16.dp))
+
+            Text("Character Left : ${characterLeft(textInput.length)}")
+
+            Spacer( modifier = Modifier.height(16.dp))
+
+            OutlinedButton(
+                onClick = {},
+                modifier = Modifier
+                    .padding(16.dp)) { Text("Save Message")
+            }
+
+            Divider(modifier = Modifier.padding(16.dp))
+
+        }
+    }
+}
+
+fun characterLeft(messageLength: Int): Int {
+    return 160 - messageLength
+}
+
+
+@Preview
+@Composable
+fun ViewPreview(){
+    WelcomeView()
+}
