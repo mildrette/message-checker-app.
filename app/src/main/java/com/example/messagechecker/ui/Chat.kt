210f65faf6c81a76e31ad9c306837aa676998d97
Mildrette <mildredfonka1@gmail.com>
2026-08-23 21:45:38 +0100

    lazycolumn

--- app/src/main/java/com/example/messagechecker/ui/Chat.kt
+++ app/src/main/java/com/example/messagechecker/ui/Chat.kt
@@ -1,82 +1,133 @@
 package com.example.messagechecker.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import com.example.messagechecker.ui.component.IconButton
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun Chat(
-//    navController: NavController
-) {
-    var textInput by remember { mutableStateOf("") }
+fun Chat() {
+    var textInput by remember {
+        mutableStateOf("")
+    }
+
+    val artist = Artist(
+        name = "Taylor Swift",
+        lastSeenOnline = "Online 5 minutes ago"
+    )
 
     Column(
         modifier = Modifier
+            .fillMaxSize()
             .background(Color.White)
             .padding(20.dp)
-            .fillMaxSize()
     ) {
 
         Text("Chat Screen")
 
-        Spacer(modifier = Modifier.height(10.dp))
+        Spacer(
+            modifier = Modifier.height(10.dp)
+        )
 
         TextField(
             value = textInput,
-            onValueChange = { textInput = it },
-            label = { Text("enter your message here") }
-
+            onValueChange = {
+                textInput = it
+            },
+            label = {
+                Text("Enter your message here")
+            }
         )
 
-        Spacer(modifier = Modifier.height(10.dp))
+        Spacer(
+            modifier = Modifier.height(10.dp)
+        )
+
+        ArtistCardRow(
+            artist = artist
+        )
+
+        Spacer(
+            modifier = Modifier.height(10.dp)
+        )
 
         Row(
             modifier = Modifier
-                .padding(20.dp)
                 .fillMaxWidth()
+                .padding(20.dp),
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.CenterVertically
         ) {
 
-            Button(onClick = {
-//            navController.navigate("details")
-            }) {
+            Button(
+                onClick = {
+                }
+            ) {
                 Text("Send")
             }
 
-            Spacer(modifier = Modifier.padding(10.dp))
+            Spacer(
+                modifier = Modifier.width(10.dp)
+            )
 
-            IconButton(onClick = {
-//            navController.popBackStack()
-            })
-
+            IconButton(
+                onClick = {
+                }
+            ) {
+                Text("Back")
+            }
         }
     }
 }
 
-@Preview
+
 @Composable
-fun ChatPreview(){
-    Chat(
-//        navController = NavController
-    )
+fun ArtistCardRow(artist: Artist) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column {
+            Text(
+                text = artist.name
+            )
+
+            Text(
+                text = artist.lastSeenOnline
+            )
+        }
+    }
+}
+
+
+data class Artist(
+    val name: String,
+    val lastSeenOnline: String
+)
+
+
+@Preview(showBackground = true)
+@Composable
+fun ChatPreview() {
+    Chat()
 }
